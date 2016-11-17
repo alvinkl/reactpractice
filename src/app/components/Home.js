c0031a71b06a6d128966dc101f1a80a418373c20
Alvin Kristanto <alvink77@gmail.com>
2016-11-17 21:09:16 +0700

    added props

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -1,17 +1,47 @@
 import React from 'react'
 
 export class Home extends React.Component {
+    constructor(props) {
+        super();
+        this.state = {
+            age: props.initialAge,
+            homeLink: "changed Link"
+        }
+    }
+
+    onMakeOlder() {
+        this.setState({
+            age: this.state.age + 3
+        })
+    }
+
+    onChangeLink() {
+        this.props.changeLink(this.state.homeLink)
+        console.log(this.state.homeLink)
+    }
+
     render() {
-        var user = this.props.user;
         return (
             <div>
-                <p>Hi, my name is {user.name}</p>
-                <p>I'm {user.age} years old</p>
-                <p>My hobbies are : </p>
-                <ul>
-                    {user.hobbies.map((h, i) => <li key={i}>{h}</li>)}
-                </ul>
+                <p>Hi, my name is {this.props.name}</p>
+                <p>I'm {this.state.age} years old</p>
+                <hr/>
+                <button
+                    className='btn btn-primary'
+                    onClick={() => this.onMakeOlder()}>Make me older!</button>
+                <button className='btn btn-primary'
+                    onClick={this.props.greet}>Greet</button>
+                <hr/>
+                <button className='btn btn-primary'
+                    onClick={() => this.onChangeLink()}>Change Link</button>
             </div>
         )
     }
+}
+
+Home.propTypes = {
+    name: React.PropTypes.string,
+    initialAge:  React.PropTypes.number,
+    greet: React.PropTypes.func,
+    changeLink: React.PropTypes.func
 }
--- src/app/index.js
+++ src/app/index.js
@@ -5,6 +5,23 @@ import { Header } from './components/Header'
 import { Home } from './components/Home'
 
 class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            homeLink: 'Home'
+        }
+    }
+
+    onGreet() {
+        alert('Hello!');
+    }
+
+    onChangeLinkName(newName) {
+        this.setState({
+            homeLink: newName
+        })
+    }
+
     render() {
         var user = {
             name: 'Sussy',
@@ -15,12 +32,16 @@ class App extends React.Component {
             <div className="container">
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Header />
+                        <Header homeLink={this.state.homeLink}/>
                     </div>
                 </div>
                 <div className="row">
                     <div className="col-xs-10 col-xs-offset-1">
-                        <Home user={ user }/>
+                        <Home
+                            name={ "suzzi" }
+                            initialAge={15}
+                            greet={this.onGreet}
+                            changeLink={() => this.onChangeLinkName()}/>
                     </div>
                 </div>
             </div>
